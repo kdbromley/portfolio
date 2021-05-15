78d358929f8f8bf9db83b97ff71ba64e77a1a117
kd Bromley <kd.bromley1@gmail.com>
2021-05-15 04:01:31 -0700

    fonts and project dummy

---
+++ src/projects-store.js
@@ -0,0 +1,28 @@
+export const STORE = {
+    "projects": [
+        {
+            "id": 1,
+            "name": 'on AUX',
+            "description": `Second fullstack capstone for Thinkful - a music sharing social media app. Frontend built 
+                with React. Backend is an Express server and PostgreSQL database. Also utilizes Spotify's API.`,
+            "tech": ['HTML', 'CSS', 'Javascript', 'React', 'Node.js', 'Express.js', 'PostgreSQL', 'SQL/Knex', 'Fetch API calls'],
+            "link": 'https://github.com/kdbromley/minder-app'
+        },
+        {
+            "id": 2,
+            "name": `'minder`,
+            "description": `First fullstack capstone for Thinkful. A reminders app built with React and backend consisting
+                of an Express server and Heroku PostgreSQL database.`,
+            "tech": ['HTML', 'CSS', 'Javascript', 'React', 'Node.js', 'Express.js', 'PostgreSQL', 'SQL/Knex'],
+            "link": 'https://github.com/kdbromley/on-aux'
+        },
+        {
+            "id": 3,
+            "name": 'noteful',
+            "description": `Fullstack project completed in two parts for Thinkful. Frontend completed as a capstone for React coursework.
+                Backend completed as part coursework on Node.js and Postgres`,
+            "tech": ['HTML', 'CSS', 'Javascript', 'React', 'Node.js', 'Express.js', 'PostgreSQL', 'SQL/Knex'],
+            "link": 'https://github.com/kdbromley/noteful'
+        },
+    ]
+}
